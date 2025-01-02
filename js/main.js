fa878e8122f8a9ae8905868cc057966ee6d76861
pulgasari <regenbogeneinhorn666@gmail.com>
2025-01-02 08:59:14 +0100

    update

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  function buildTableOfContents() {
+    const menu = document.getElementById('toc');
+    if (!menu) return;
+
+    const headlines = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    const toc = document.createElement('ul');
+
+    headlines.forEach(headline => {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      const id = headline.innerText.replace(/\s+/g, '-').toLowerCase();
+      
+      headline.id = id;
+      link.href = `#${id}`;
+      link.innerText = headline.innerText;
+
+      listItem.appendChild(link);
+      toc.appendChild(listItem);
+    });
+
+    menu.appendChild(toc);
+  }
+
+  buildTableOfContents();
+});
