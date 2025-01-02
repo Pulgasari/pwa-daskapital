4b1e21941c55991dec5f5eafa4949eff9553951a
pulgasari <regenbogeneinhorn666@gmail.com>
2025-01-02 16:52:39 +0100

    update main.js

--- js/main.js
+++ js/main.js
@@ -35,3 +35,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buildTableOfContents();
 });
+
+
+
+const x = selector => {
+  
+  let el = document.querySelector( selector );
+  let obj = { el };
+
+  // Class
+  obj.addClass    = function(name) { this.el.classList.add(name);    return this; };
+  obj.removeClass = function(name) { this.el.classList.remove(name); return this; };
+  obj.toggleClass = function(name) { this.el.classList.toggle(name); return this; };
+
+  // Log
+  obj.log = function() { console.log(this.el); return this; };
+
+  return obj;
+};
